99208977b43ab62b9be7b28369a4cfc0ad5eea5d
dochi486 <93mskim@gmail.com>
2023-05-28 02:07:54 +0900

    개인정보 보호 약관 문제 푸는 중

--- TermsOfPrivacy/Privacy.cs
+++ TermsOfPrivacy/Privacy.cs
@@ -8,6 +8,11 @@ namespace TermsOfPrivacy
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Solution solution = new Solution();
+            string today = "2022.05.19";
+            string[] terms = { "A 6", "B 12", "C 3" };
+            string[] privacies = { "2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C" };
+            solution.solution(today, terms, privacies);
         }
     }
 
@@ -18,7 +23,7 @@ namespace TermsOfPrivacy
             int[] answer = new int[] { };
             Dictionary<string, string> privacyTerms = new Dictionary<string, string>();
             Dictionary<string, string> termsConditions = new Dictionary<string, string>();
-            
+            string condition = "A";
 
             // terms 유효기간
             for (int i = 0; i < terms.Length; i++)
@@ -31,6 +36,7 @@ namespace TermsOfPrivacy
                 }
                 else
                 {
+                    condition = terms[0];
                     termsConditions.Add(term[0], term[1]);
                 }
             }
@@ -47,12 +53,31 @@ namespace TermsOfPrivacy
                 else
                 {
                     privacyTerms.Add(privacy[1], privacy[0]);
+
+
                 }
             }
 
-
+            // today
+           
             var date = today.Split(".");
+            var year = date[0];
+            var month = date[1];
+            var day = date[2];
             
+
+            //privacyTerms 날짜 분해할지 고민
+            {
+                // A 6
+                // B 12
+                // C 3
+                
+                // 5 -06
+                // 07 - 12 
+                // 
+                year - privacyTerms[termsConditions[]
+
+            }
 
 
 
