394910a266cd31790e10866c156afe514f1364fa
dochi486 <93mskim@gmail.com>
2023-05-28 23:02:25 +0900

    풀었는데 이중포문, 배열인덱스 수정으로 시간복잡도 통과 못함

---
+++ Run/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Solution solution = new Solution();
+            string[] players = { "mumu", "soe", "poe", "kai", "mine" };
+            string[] callings = { "kai", "kai", "mine", "mine" };
+            solution.solution(players, callings);
+        }
+    }
+
+    public class Solution
+    {
+        public string[] solution(string[] players, string[] callings)
+        {
+            Dictionary<int, string> playerRanks = new Dictionary<int, string>();
+
+            var tempLength = players.Length;
+            for (int j = 0; j < tempLength; j++)
+            {
+                playerRanks.Add(j, players[j]);
+
+                // 플레이어 순위가 담긴 players 배열의 요소 중에서
+                //for (int i = 1; i < players.Length; i++)
+                //{
+                //    // player의 요소를 callings에서 찾았다면
+                //    if (players[i] == callings[j])
+                //    {
+                //        // players에서도 한 칸씩 앞으로 당겨준다.... 
+                //        var temp = players[i];
+                //        players[i] = players[i - 1];
+                //        players[i - 1] = temp;
+                //    }
+                //}
+            }
+
+            return players;
+        }
+    }
+}
